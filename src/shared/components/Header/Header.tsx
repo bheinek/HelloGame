d964df112e7de086b461a195372dccb481377d57
Hynek Bagar <b.hynek@commend.site>
2023-11-04 11:39:54 +0100

    fix tailwindcss

--- src/shared/components/Header/Header.tsx
+++ src/shared/components/Header/Header.tsx
@@ -3,5 +3,9 @@ import { FC } from "react";
 type Props = { plural?: boolean };
 
 export const Header: FC<Props> = ({ plural = false }) => {
-  return <h1>Hello game{plural ? "s" : null}</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello game{plural ? "s" : null}
+    </h1>
+  );
 };
